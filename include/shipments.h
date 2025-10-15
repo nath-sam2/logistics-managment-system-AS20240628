42569d939d2f811371e7acc8cb814c8a0b682408
nathali-sam <as20240628@sci.sjp.ac.lk>
2025-10-15 17:39:18 +0530

    Day 03: Implemented city managemet system

--- include/shipments.h
+++ include/shipments.h
@@ -2,14 +2,14 @@
 #define SHIPMENTS_H_INCLUDED
 
 #define MAX_CITIES 30
+#define MAX_NAME_LEN 50
+
+char cities[MAX_CITIES][MAX_NAME_LEN];
+int currentCityCount;
 
 void addCity();
 void renameCity();
 void removeCity();
 void displayCities();
-void editDistance();
-void displayDistanceTable();
-void saveRoutesToFile(char path[]);
-int loadDeliveriesFromFile(char path[]);
 
 #endif // SHIPMENTS_H_INCLUDED
