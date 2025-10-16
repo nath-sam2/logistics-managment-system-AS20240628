d8b39939db8744aa2ed38efcd80b1f4f4ed20307
nathali-sam <as20240628@sci.sjp.ac.lk>
2025-10-16 19:47:23 +0530

    Added vehicle management system with arrays (display+initialization).

--- include/vehicles.h
+++ include/vehicles.h
@@ -1,10 +1,16 @@
-#ifndef VEHICLES_H_INCLUDED
-#define VEHICLES_H_INCLUDED
+#ifndef VEHICLES_H
+#define VEHICLES_H
 
+#define MAX_VEHICLES 3
+#define MAX_VEHICLE_NAME 30
+
+char vehicleNames[MAX_VEHICLES][MAX_VEHICLE_NAME];
+double vehicleRatePerKm[MAX_VEHICLES];
+double vehicleCapacity[MAX_VEHICLES];
+double vehicleSpeed[MAX_VEHICLES];
+double vehicleFuelEfficiency[MAX_VEHICLES];
+
+void initializeVehicles();
 void displayVehicles();
-int getVehicleRate(int index);
-int getVehicleCapacity(int index);
-int getVehicleSpeed(int index);
-int getVehicleEfficiency(int index);
 
-#endif // VEHICLES_H_INCLUDED
+#endif
